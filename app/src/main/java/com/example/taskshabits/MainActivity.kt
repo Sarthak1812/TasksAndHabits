685e8aceaa2744d0350450dcf4359fa4a7d81516
Sarthak Musmade <sarthakmusmade@gmail.com>
2022-01-13 17:32:03 +0530

    Initial commit

--- app/src/main/java/com/example/taskshabits/MainActivity.kt
+++ app/src/main/java/com/example/taskshabits/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.taskshabits
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val catHabitsBtn = findViewById<Button>(R.id.btn_cat_habits)
+        catHabitsBtn.setOnClickListener {
+            val toast = Toast.makeText(this, "ButtonClicked", Toast.LENGTH_SHORT)
+            toast.show()
+        }
     }
 }
