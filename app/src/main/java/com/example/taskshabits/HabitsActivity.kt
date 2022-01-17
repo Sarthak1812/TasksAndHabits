7da52fac26b96f0078f4aab7f89660c41beb341f
Sarthak Musmade <sarthakmusmade@gmail.com>
2022-01-17 21:43:42 +0530

    add - HabitsAdapter

--- app/src/main/java/com/example/taskshabits/HabitsActivity.kt
+++ app/src/main/java/com/example/taskshabits/HabitsActivity.kt
@@ -10,42 +10,58 @@ import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class HabitsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_habits)
 
-        // row/card onClick listener
-//        val habitCard = findViewById<CardView>(R.id.cv_habits)
-//        habitCard.setOnClickListener {
-//            // creating dialog
-//            val habitDialog = Dialog(this)
-//            habitDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-//            habitDialog.setCancelable(false)
-//            habitDialog.setContentView(R.layout.habits_dialog)
-//
-//            // init
-//            val doneDialogBtn = habitDialog.findViewById<Button>(R.id.btn_habit_dialog_done)
-//            val doneDialogTV = habitDialog.findViewById<TextView>(R.id.tv_habit_dialog_done)
-//            val closeDialogBtn = habitDialog.findViewById<ImageButton>(R.id.btn_habit_dialog_close)
-//
-//            // changing/replacing the DoneBtn onClick with DoneTextView
-//            doneDialogBtn.visibility = View.VISIBLE
-//            doneDialogTV.visibility = View.GONE
-//            doneDialogBtn.setOnClickListener {
-//                doneDialogTV.visibility = View.VISIBLE
-//                doneDialogBtn.visibility = View.GONE
-//            }
-//
-//            // onClick - Close ImageButton
-//            closeDialogBtn.setOnClickListener {
-//                habitDialog.dismiss()
-//                Toast.makeText(this, "Closed", Toast.LENGTH_SHORT).show()
-//            }
-//
-//            // show dialog
-//            habitDialog.show()
-//        }
+        val habitsRV = findViewById<RecyclerView>(R.id.rv_habits)
+        habitsRV.layoutManager = LinearLayoutManager(this)
+
+        // sample data
+        val sampleData = ArrayList<HabitsItemViewModel>()
+
+        for (i in 1..5){
+            sampleData.add(HabitsItemViewModel("WORKOUT", "10 days left", android.R.drawable.star_big_on))
+        }
+
+        val habitsAdapter = HabitsAdapter(sampleData)
+        habitsRV.adapter = habitsAdapter
+
+
+/*         row/card onClick listener
+        val habitCard = findViewById<CardView>(R.id.cv_habits)
+        habitCard.setOnClickListener {
+            // creating dialog
+            val habitDialog = Dialog(this)
+            habitDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            habitDialog.setCancelable(false)
+            habitDialog.setContentView(R.layout.habits_dialog)
+
+            // init
+            val doneDialogBtn = habitDialog.findViewById<Button>(R.id.btn_habit_dialog_done)
+            val doneDialogTV = habitDialog.findViewById<TextView>(R.id.tv_habit_dialog_done)
+            val closeDialogBtn = habitDialog.findViewById<ImageButton>(R.id.btn_habit_dialog_close)
+
+            // changing/replacing the DoneBtn onClick with DoneTextView
+            doneDialogBtn.visibility = View.VISIBLE
+            doneDialogTV.visibility = View.GONE
+            doneDialogBtn.setOnClickListener {
+                doneDialogTV.visibility = View.VISIBLE
+                doneDialogBtn.visibility = View.GONE
+            }
+
+            // onClick - Close ImageButton
+            closeDialogBtn.setOnClickListener {
+                habitDialog.dismiss()
+                Toast.makeText(this, "Closed", Toast.LENGTH_SHORT).show()
+            }
+
+            // show dialog
+            habitDialog.show()
+        } */
     }
 }
--- app/src/main/java/com/example/taskshabits/HabitsAdapter.kt
+++ app/src/main/java/com/example/taskshabits/HabitsAdapter.kt
@@ -3,6 +3,8 @@ package com.example.taskshabits
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class HabitsAdapter(private val mHabitsList: List<HabitsItemViewModel>) :RecyclerView.Adapter<HabitsAdapter.ViewHolder>(){
@@ -15,16 +17,21 @@ class HabitsAdapter(private val mHabitsList: List<HabitsItemViewModel>) :Recycle
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-//        val HabitsItemViewModel = mHabitsList[position]
-//        holder.
+        val HabitsItemViewModel = mHabitsList[position]
+        holder.habitsTitle.text = HabitsItemViewModel.cardTitle
+        holder.habitsDaysCount.text = HabitsItemViewModel.cardDaysCount
+        holder.habitsImage.setImageResource(HabitsItemViewModel.cardImage)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        //return number of items in the list
+        return mHabitsList.size
     }
 
     // Holds the views for adding it to image and text
     class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
-
+        val habitsTitle: TextView = itemView.findViewById(R.id.tv_habits_card_title)
+        val habitsDaysCount: TextView = itemView.findViewById(R.id.tv_habits_card_day_count)
+        val habitsImage: ImageView = itemView.findViewById(R.id.iv_habits_card)
     }
 }
