1ee9448e28d9758ccb2d9795328cd060b1f99a2e
Sarthak Musmade <sarthakmusmade@gmail.com>
2022-01-19 21:17:47 +0530

    changes to habits dialog

--- app/src/main/java/com/example/taskshabits/HabitsActivity.kt
+++ app/src/main/java/com/example/taskshabits/HabitsActivity.kt
@@ -27,32 +27,30 @@ class HabitsActivity : AppCompatActivity() {
         mHabitsViewModel = ViewModelProvider(this).get(HabitsViewModel::class.java)
 
         // sample data
-//        val sampleData = ArrayList<Habits>()
-//
-//        sampleData.add(Habits(1, "WORKOUT", "10 days left", android.R.drawable.star_big_on))
-//        sampleData.add(Habits(2, "WORKOUT", "10 days left", android.R.drawable.star_big_on))
-//        sampleData.add(Habits(3, "WORKOUT", "10 days left", android.R.drawable.star_big_on))
+        val sampleData = ArrayList<Habits>()
+
+        sampleData.add(Habits(1, "WORKOUT", "10 days left", android.R.drawable.star_big_on))
+        sampleData.add(Habits(2, "WORKOUT", "10 days left", android.R.drawable.star_big_on))
+        sampleData.add(Habits(3, "WORKOUT", "10 days left", android.R.drawable.star_big_on))
 
         // RecyclerView
         val habitsRV = findViewById<RecyclerView>(R.id.rv_habits)
         val habitsAdapter = HabitsAdapter()
         habitsRV.layoutManager = LinearLayoutManager(this)
         habitsRV.adapter = habitsAdapter
-//        habitsAdapter.setHabits(sampleData)
+        habitsAdapter.setHabits(sampleData)
 
         // TODO(Change habit dialog box to both Update and Add DATA .... !!!! )
 
         // habits card item onClick listener
         habitsAdapter.setOnHabitsItemClickListener(object : HabitsAdapter.OnHabitsItemClickListener{
             override fun onHabitsItemClick(position: Int) {
-                Toast.makeText(this@HabitsActivity, "clicked", Toast.LENGTH_SHORT).show()
-                habitsDialogCall()
+                HabitsDialogClass(this@HabitsActivity).show()
             }
-
         })
 
         addHabitBtn.setOnClickListener {
-            insertDataToDb()
+//            insertDataToDb()
 //            sampleData.add(Habits(4,"WORKOUT", "10 days left", android.R.drawable.star_big_on))
 //            habitsAdapter.setHabits(sampleData)
         }
@@ -60,39 +58,6 @@ class HabitsActivity : AppCompatActivity() {
 
 
 
-    }
-
-    fun habitsDialogCall(){
-        //row/card onClick listener
-        // creating dialog
-        val habitsDialog = Dialog(this)
-        habitsDialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-        habitsDialog.setCancelable(false)
-        habitsDialog.setContentView(R.layout.habits_dialog)
-
-        // init
-        val doneDialogBtn = habitsDialog.findViewById<Button>(R.id.btn_habit_dialog_done)
-        val doneDialogTV = habitsDialog.findViewById<TextView>(R.id.tv_habit_dialog_done)
-        val doneContainerLL = habitsDialog.findViewById<LinearLayout>(R.id.ll_habit_done_container)
-        val closeDialogBtn = habitsDialog.findViewById<ImageButton>(R.id.btn_habit_dialog_close)
-
-        // changing/replacing the DoneBtn onClick with DoneTextView
-        doneDialogBtn.visibility = View.VISIBLE
-        doneDialogTV.visibility = View.GONE
-        doneDialogBtn.setOnClickListener {
-            TransitionManager.beginDelayedTransition(doneContainerLL)
-            doneDialogTV.visibility = View.VISIBLE
-            doneDialogBtn.visibility = View.GONE
-        }
-
-        // onClick - Close ImageButton
-        closeDialogBtn.setOnClickListener {
-            habitsDialog.dismiss()
-            Toast.makeText(this, "Closed", Toast.LENGTH_SHORT).show()
-        }
-
-        // show dialog
-        habitsDialog.show()
     }
 
     private fun insertDataToDb() {
---
+++ app/src/main/java/com/example/taskshabits/HabitsDialogClass.kt
@@ -0,0 +1,48 @@
+package com.example.taskshabits
+
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.transition.TransitionManager
+import android.view.View
+import android.view.Window
+import android.widget.*
+
+class HabitsDialogClass(context: Context) : Dialog(context){
+
+    init {
+        setCancelable(false)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(R.layout.habits_dialog)
+
+        // init
+        val titleEt = findViewById<EditText>(R.id.et_habits_title)
+        val daysCountEt = findViewById<EditText>(R.id.et_habits_days_count)
+        val doneDialogBtn = findViewById<Button>(R.id.btn_habit_dialog_done)
+        val doneDialogTV = findViewById<TextView>(R.id.tv_habit_dialog_done)
+        val doneContainerLL = findViewById<LinearLayout>(R.id.ll_habit_done_container)
+        val closeDialogBtn = findViewById<ImageButton>(R.id.btn_habit_dialog_close)
+
+        // changing/replacing the DoneBtn onClick with DoneTextView
+        doneDialogBtn.visibility = View.VISIBLE
+        doneDialogTV.visibility = View.GONE
+        doneDialogBtn.setOnClickListener {
+            TransitionManager.beginDelayedTransition(doneContainerLL)
+            doneDialogTV.visibility = View.VISIBLE
+            doneDialogBtn.visibility = View.GONE
+        }
+
+        // onClick - Close ImageButton
+        closeDialogBtn.setOnClickListener {
+            dismiss()
+            Toast.makeText(context, "Closed", Toast.LENGTH_SHORT).show()
+        }
+
+
+    }
+
+}
