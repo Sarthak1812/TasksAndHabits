7397add6c362d9a97bab9ccdc8986751483fef66
Sarthak Musmade <sarthakmusmade@gmail.com>
2022-01-22 17:45:28 +0530

    added tagAnimation with LottieAnimationView

--- app/src/main/java/com/example/taskshabits/fragment/HabitsDialogFragment.kt
+++ app/src/main/java/com/example/taskshabits/fragment/HabitsDialogFragment.kt
@@ -2,12 +2,14 @@ package com.example.taskshabits.fragment
 
 import android.os.Bundle
 import android.transition.TransitionManager
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.ViewModelProvider
+import com.airbnb.lottie.LottieAnimationView
 import com.example.taskshabits.R
 import com.example.taskshabits.data.Habits
 import com.example.taskshabits.util.HabitsViewModel
@@ -26,6 +28,7 @@ class HabitsDialogFragment: DialogFragment(){
         // init
         val titleEt = rootView.findViewById<EditText>(R.id.et_habits_title)
         val daysCountEt = rootView.findViewById<EditText>(R.id.et_habits_days_count)
+        val tagAnimLottie = rootView.findViewById<LottieAnimationView>(R.id.lottie_anim_view_tag)
         val autoCompleteTagsTv = rootView.findViewById<AutoCompleteTextView>(R.id.autoComplete_tags)
         val doneDialogBtn = rootView.findViewById<Button>(R.id.btn_habit_dialog_done)
         val doneDialogTV = rootView.findViewById<TextView>(R.id.tv_habit_dialog_done)
@@ -38,7 +41,9 @@ class HabitsDialogFragment: DialogFragment(){
         val tags = resources.getStringArray(R.array.habits_tag_array)
         val tagsArrayAdapter = ArrayAdapter(requireContext(), R.layout.dropdownmenu_item, tags)
         autoCompleteTagsTv.setAdapter(tagsArrayAdapter)
-
+        autoCompleteTagsTv.setOnItemClickListener { parent, view, position, id ->
+            setTagAnimation(autoCompleteTagsTv.text.toString(), tagAnimLottie)
+        }
 
         // changing/replacing the DoneBtn onClick with DoneTextView
         doneDialogBtn.visibility = View.VISIBLE
@@ -59,6 +64,20 @@ class HabitsDialogFragment: DialogFragment(){
 
         return rootView
     }
+
+    private fun setTagAnimation(tagChoice: String, animTagView: LottieAnimationView) {
+        when(tagChoice) {
+            "Workout/Fitness" -> animTagView.setAnimation(R.raw.tag_workout)
+            "Work/Task" -> animTagView.setAnimation(R.raw.tag_work)
+            "Yoga/Meditation" -> animTagView.setAnimation(R.raw.tag_yoga)
+            "Default-Target" -> animTagView.setAnimation(R.raw.tag_default_target)
+            else -> {
+                animTagView.setAnimation(R.raw.tag_default_target)
+            }
+        }
+        animTagView.playAnimation()
+    }
+
 
     private fun insertDataIntoDb() {
         // TODO("Add Function for Input Check ")
